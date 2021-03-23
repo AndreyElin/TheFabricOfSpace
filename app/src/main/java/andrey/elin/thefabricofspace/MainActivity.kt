8ba1dcba2c434834a7cc607cfa23eba12a4ad5d3
Andrey Elin <musterspb@mail.ru>
2021-03-23 19:37:00 +0300

    Initial commit

---
+++ app/src/main/java/andrey/elin/thefabricofspace/MainActivity.kt
@@ -0,0 +1,11 @@
+package andrey.elin.thefabricofspace
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
