525e3942cca8b8449b6c2b3f88be4366d9a379b2
Andrey Elin <musterspb@mail.ru>
2021-04-04 17:48:13 +0300

    Home work 2

--- app/src/main/java/andrey/elin/thefabricofspace/SettingFragment.kt
+++ app/src/main/java/andrey/elin/thefabricofspace/SettingFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.chip.Chip
